0da3bb67f2bd6adbc33b666cec9e1c3ef622bdd2
Watthana Kayowaen <Jameskzyy@gmail.com>
2024-01-06 16:29:23 +0700

    Create main.js

---
+++ testfigma/main.js
@@ -0,0 +1,22 @@
+console.log("Watthana")
+console.log("hello")
+
+var namee = "jame"
+console.log(namee)
+namee="donut"
+console.log(namee)
+
+var first="Watthana"
+console.log(typeof "WW")
+
+var x=15
+{
+    var x=2
+    let y=10
+    console.log(y)
+}
+console.log(x)
+
+let Name=prompt("โปรดกรอก")
+console.log(Name)
+console.log(Math.floor(Math.random()*10));
